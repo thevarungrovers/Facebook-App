f94514d30edf0f9a92b57f7afa7176bb806d4f27
Varun Grover <grover82vinni@gmail.com>
2021-06-28 21:03:04 +0530

    Toast feature

--- app/src/main/java/com/example/facebookapp/MainActivity.java
+++ app/src/main/java/com/example/facebookapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
-import android.view.View;
+//import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        create account
         Button createAccount = findViewById(R.id.createAccount_button_id);
-        createAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, register_activity.class);
-                startActivity(intent);
+        createAccount.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, register_activity.class);
+            startActivity(intent);
+        });
+
+//        login
+        EditText UserName = (EditText) findViewById(R.id.username_input_id);
+        EditText Password = (EditText) findViewById(R.id.password_input_id);
+
+        Button btn = (Button) findViewById(R.id.login_btn_id);
+
+        btn.setOnClickListener(v -> {
+            String username = UserName.getText().toString();
+            String password = Password.getText().toString();
+
+            if(username.equals("hello") && password.equals("hello")){
+                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_LONG).show();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/facebookapp/register_activity.java
+++ app/src/main/java/com/example/facebookapp/register_activity.java
@@ -3,12 +3,46 @@ package com.example.facebookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+//import android.view.View;
+import android.widget.Button;
+//import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class register_activity extends AppCompatActivity {
+
+    EditText name, phone, email, password;
+    Button signup;
+//    DatePicker dob;
+
+    String inputName, inputEmail, inputPhone, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        name.findViewById(R.id.name_input_id);
+        inputName = name.getText().toString();
+
+        phone.findViewById(R.id.phone_input_id);
+        inputPhone = phone.getText().toString();
+
+        email.findViewById(R.id.email_input_id);
+        inputEmail = email.getText().toString();
+
+        password.findViewById(R.id.password_input_id);
+        inputPassword = password.getText().toString();
+
+//        dob.findViewById(R.id.dob_input_id);
+//        inputDob = dob.getD();
+
+        signup.setOnClickListener(v -> {
+            if(!inputName.equals("")){
+                Toast.makeText(getApplicationContext(),"All Fields are required", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
