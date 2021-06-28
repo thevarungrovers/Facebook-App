be3d2c95c2106c5e220821fc1475967286f9a906
Varun Grover <grover82vinni@gmail.com>
2021-06-28 20:20:56 +0530

    new account, logo
    1. activity_register -  added
    2. logo changed to facebook from android

--- app/src/main/java/com/example/facebookapp/MainActivity.java
+++ app/src/main/java/com/example/facebookapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.facebookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createAccount = findViewById(R.id.createAccount_button_id);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, register_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
