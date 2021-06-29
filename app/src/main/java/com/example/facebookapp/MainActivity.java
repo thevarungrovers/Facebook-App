81e319fbd070edce7b6c50f400a8538d5ca54880
Varun Grover <grover82vinni@gmail.com>
2021-06-29 15:50:30 +0530

    functionality added

--- app/src/main/java/com/example/facebookapp/MainActivity.java
+++ app/src/main/java/com/example/facebookapp/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.facebookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.content.Intent;
-//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,25 +14,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        create account
-        Button createAccount = findViewById(R.id.createAccount_button_id);
+//        create account page
+        Button createAccount = findViewById(R.id.createAccount_button_id);  //  object creation and mapping id
+           //  on click event | Create new account button
         createAccount.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, register_activity.class);
             startActivity(intent);
         });
 
-//        login
-        EditText UserName = (EditText) findViewById(R.id.username_input_id);
-        EditText Password = (EditText) findViewById(R.id.password_input_id);
+//        login page
+        EditText username = (EditText) findViewById(R.id.username_input_id); //  object creation and mapping id
+        EditText password = (EditText) findViewById(R.id.password_input_id); //  object creation and mapping id
 
-        Button btn = (Button) findViewById(R.id.login_btn_id);
+        Button btn = (Button) findViewById(R.id.login_btn_id); //  object creation and mapping id
 
+            //   on click event | LOG IN Button
         btn.setOnClickListener(v -> {
-            String username = UserName.getText().toString();
-            String password = Password.getText().toString();
+            String input_username = username.getText().toString(); // getting input from frontend
+            String input_password = password.getText().toString(); // getting input from frontend
 
-            if(username.equals("hello") && password.equals("hello")){
-                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_LONG).show();
+            // empty input fields
+            if(input_username.equals("") || input_username.equals(" ") || input_password.equals("") || input_password.equals(" ")){
+                Toast.makeText(getApplicationContext(),"Please enter the both fields", Toast.LENGTH_LONG).show(); // Notification
+            }
+            // correct info
+            else if(input_username.equals("JohnDoe") && input_password.equals("JohnDoe")){
+                Toast.makeText(getApplicationContext(),"Login Successfully", Toast.LENGTH_LONG).show(); // Notification
+            }
+            // incorrect info
+            else{
+                Toast.makeText(getApplicationContext(),"Invalid Credentials", Toast.LENGTH_LONG).show(); // Notification
             }
         });
 
--- app/src/main/java/com/example/facebookapp/register_activity.java
+++ app/src/main/java/com/example/facebookapp/register_activity.java
@@ -1,9 +1,6 @@
 package com.example.facebookapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-//import android.view.View;
 import android.widget.Button;
 //import android.widget.DatePicker;
 import android.widget.EditText;
@@ -11,37 +8,51 @@ import android.widget.Toast;
 
 public class register_activity extends AppCompatActivity {
 
-    EditText name, phone, email, password;
-    Button signup;
 //    DatePicker dob;
-
-    String inputName, inputEmail, inputPhone, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_register); // connect with xml
+
+//        Create new account
+        //  object creation and mapping id
+        EditText name = (EditText) findViewById(R.id.name_input_id);
+        EditText phone = (EditText) findViewById(R.id.phone_input_id);
+        EditText email = (EditText) findViewById(R.id.email_input_id);
+        EditText password = (EditText) findViewById(R.id.newPassword_input_id);
+        Button createAccount = (Button) findViewById(R.id.create_button_id);
 
 
-        name.findViewById(R.id.name_input_id);
-        inputName = name.getText().toString();
-
-        phone.findViewById(R.id.phone_input_id);
-        inputPhone = phone.getText().toString();
-
-        email.findViewById(R.id.email_input_id);
-        inputEmail = email.getText().toString();
-
-        password.findViewById(R.id.password_input_id);
-        inputPassword = password.getText().toString();
 
 //        dob.findViewById(R.id.dob_input_id);
 //        inputDob = dob.getD();
 
-        signup.setOnClickListener(v -> {
-            if(!inputName.equals("")){
-                Toast.makeText(getApplicationContext(),"All Fields are required", Toast.LENGTH_LONG).show();
+//            on click listener | create new account button
+        createAccount.setOnClickListener(v -> {
+            //        strings to save input from frontend
+            String input_name = name.getText().toString();
+            String input_phone = phone.getText().toString();
+            String input_email = email.getText().toString();
+            String input_password = password.getText().toString();
+
+            // to get the values from calender
+//            DatePicker datePicker = (DatePicker) findViewById(R.id.dob_input_id);
+//            int day = datePicker.getDayOfMonth();
+//            int month = datePicker.getMonth() + 1;
+//            int year = datePicker.getYear();
+
+
+
+            // empty fields
+            if(input_name.equals("") || input_name.equals(" ") || input_phone.equals("")|| input_phone.equals(" ") || input_email.equals("") || input_email.equals(" ") || input_password.equals("") || input_password.equals(" ")){
+                Toast.makeText(getApplicationContext(),"Please Enter the All the fields", Toast.LENGTH_LONG).show();
             }
+            // all entered data
+            else{
+                Toast.makeText(getApplicationContext()," Hello... " + input_name + "  Your Account Created Successfully", Toast.LENGTH_LONG).show();
+            }
+
         });
 
     }
